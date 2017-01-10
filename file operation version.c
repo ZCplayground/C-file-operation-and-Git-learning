0c291d0ea2015229c85abfc9f9430e6942bbd839
ZCplayground <250151700@qq.com>
2017-01-10 23:10:15 +0800

    git use and file operation

---
+++ file operation version.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int Lines(const char * FileName)
+{
+	FILE *fp;
+	errno_t err;
+	err = fopen_s(&fp, FileName, "r");
+	if (err == 0)
+	{
+		static int count = 0, c;
+		while ((c = fgetc(fp)) != EOF)
+		{
+			if (c == '\n') count++;
+		}
+		fclose(fp);
+		return count;
+	}
+	else
+	{
+		printf("failed to open this file\n");
+		return 0;
+	}
+}
+
+int main()
+{
+	char filename[FILENAME_MAX];
+	printf("please input file name:");
+	while (gets_s(filename)) {
+		int result = Lines(filename);
+		if (result)
+			printf("line(s) of codes accumulated: %d\n", result);
+		printf("please input file name:(or ctrl+z to exit):");
+	}
+	return 0;
+}
